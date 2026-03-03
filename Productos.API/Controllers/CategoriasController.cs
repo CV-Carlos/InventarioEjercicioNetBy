09f5544b1b18389b6fce85468216160a063f9a76
ccerd <ccerd@DESKTOP-JMA1LVA>
2026-03-03 14:55:04 -0500

    -feat: Controlador para el apartado de categorias

---
+++ Productos.API/Controllers/CategoriasController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Productos.API.Data;
+
+namespace Productos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly ProductosDbContext _context;
+
+        public CategoriasController(ProductosDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var categorias = await _context.Categorias
+                .OrderBy(c => c.Nombre)
+                .Select(c => new { c.Id, c.Nombre })
+                .ToListAsync();
+            return Ok(categorias);
+        }
+    }
+}
